9d13e03a056d6065d3a5422cfdff4a1d5e2b1202
unknown <adnan.noorullah@gmail.com>
2022-11-12 00:19:49 -0600

    Setting up outline for frontend

---
+++ front-end/src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css';
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
+import Home from './Components/Home';
+
+function App() {
+  return (
+    <Router>
+      
+
+    </Router>
+  );
+}
+
+export default App;
